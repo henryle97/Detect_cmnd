b81541f30536b23e21f359ed993b3febe2d7c7b2
Hisiter <leeyueshing19@gmail.com>
2020-08-20 23:48:40 +0700

    install

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 conda env create -f environment.yml
-
-cd src/lib/models/networks/
+conda activate py36_torch1.4
+cd models/networks/
 rm -rf DCNv2
 git clone https://github.com/CharlesShang/DCNv2.git
 cd DCNv2
 ./make.sh
-conda activate
